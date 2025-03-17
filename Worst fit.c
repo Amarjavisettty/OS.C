39e4fe62aac5e99b4661774c1e92f6eb062842ef
Amarjavisettty <23pa1a0592@vishnu.edu.in>
2025-03-17 12:02:58 +0530

    Create Worst fit.c

---
+++ Worst fit.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#define max 25
+void main() {
+    int frag[max], b[max], f[max], i, j, nb, nf, temp, highest = 0;
+    static int bf[max], ff[max];
+    printf("\n\tMemory Management Scheme - Worst Fit");
+    printf("\nEnter the number of blocks: ");
+    scanf("%d", &nb);
+    printf("Enter the number of files: ");
+    scanf("%d", &nf);
+    printf("\nEnter the size of the blocks:-\n");
+    for(i = 1; i <= nb; i++) {
+        printf("Block %d: ", i);
+        scanf("%d", &b[i]);
+    }
+    printf("Enter the size of the files :-\n");
+    for(i = 1; i <= nf; i++) {
+        printf("File %d: ", i);
+        scanf("%d", &f[i]);
+    }
+    for(i = 1; i <= nf; i++) {
+        highest = 0;
+        for(j = 1; j <= nb; j++) {
+            if(bf[j] != 1) {
+                temp = b[j] - f[i];
+                if(temp >= 0 && temp > highest) {
+                    ff[i] = j;
+                    highest = temp;
+                }
+            }
+        }
+        frag[i] = highest;
+        bf[ff[i]] = 1;
+    }
+    printf("\nFile No\tFile Size\tBlock No\tBlock Size\tFragment\n");
+    for(i = 1; i <= nf; i++) {
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i, f[i], ff[i], b[ff[i]], frag[i]);
+    }
+    getchar();
+    getchar();
+}
+output
+	Memory Management Scheme - Worst Fit
+Enter the number of blocks: 3
+Enter the number of files: 2
+
+Enter the size of the blocks:-
+Block 1: 5
+Block 2: 2
+Block 3: 7
+Enter the size of the files :-
+File 1: 1
+File 2: 4
+
+File No	File Size	Block No	Block Size	Fragment
+1		1		3		7		6
+2		4		1		5		1
